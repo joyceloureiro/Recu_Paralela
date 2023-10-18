1a1de1ed292b346aa0572678906ea367bdb610af
joyce <saoeiisamaa@gmail.com>
2023-10-18 19:19:38 -0300

    Feat- Recupeeração paralela.

--- Recu_Paralela/Form1.cs
+++ Recu_Paralela/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Recu_Paralela
 {
-    public partial class Form1 : Form
+    public partial class FormAdvocacia : Form
     {
-        public Form1()
+        public FormAdvocacia()
         {
             InitializeComponent();
         }
--- Recu_Paralela/Program.cs
+++ Recu_Paralela/Program.cs
@@ -16,7 +16,7 @@ namespace Recu_Paralela
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormAdvocacia());
         }
     }
 }
